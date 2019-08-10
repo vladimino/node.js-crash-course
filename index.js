dc991f6ce2fa1d2a83558bf4b00bf852b62b473a
Vlad Bilokur <vladimino@gmail.com>
2019-08-10 15:32:34 +0200

    Init web server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Home</h1>');
+  } else {
+    res.end('<h2>Why are you here?</h2>');
+  }
+});
 
-logger.on('message', (data) => console.log('Called Listener:', data));
+const PORT = process.env.PORT || 5000;
 
-logger.log('Hello World');
-logger.log('Hi');
-logger.log('Hello');
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
