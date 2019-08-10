4938064e7b60f9851008550b89da73f2b6e7fca9
Vlad Bilokur <vladimino@gmail.com>
2019-08-10 19:45:25 +0200

    Make server dynamic

--- index.js
+++ index.js
@@ -3,31 +3,41 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,data) => {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
+
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  let extName = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extName) {
+    case '.js':
+      contentType = 'application/json';
+      break;
+    case '.html':
+    default:
+      contentType = 'text/html';
   }
 
-  if (req.url === '/about') {
-    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,data) => {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
-  }
-
-  if (req.url === '/api/users') {
-    const users = [
-      {name: 'Bob', age: 40},
-      {name: 'Alice', age: 25}
-    ];
-
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(users));
-  }
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+       if (err.code == 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) => {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.end(data, 'utf8');
+        });
+       } else {
+        res.writeHead(500, {'Content-Type': 'text/html'});
+        res.end(`Server Error ${error.code}`);
+      }
+    } else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(data, 'utf8');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
