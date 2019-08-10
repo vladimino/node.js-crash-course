9698adeef4585c0138ae47c2f30604ea895a3ca0
Vlad Bilokur <vladimino@gmail.com>
2019-08-10 15:40:01 +0200

    Extract html to files and add API mock

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Home</h1>');
-  } else {
-    res.end('<h2>Why are you here?</h2>');
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,data) => {
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    });
+  }
+
+  if (req.url === '/about') {
+    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,data) => {
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    });
+  }
+
+  if (req.url === '/api/users') {
+    const users = [
+      {name: 'Bob', age: 40},
+      {name: 'Alice', age: 25}
+    ];
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(users));
   }
 });
 
